98a5297bb2fc046ea5f89f2a7512935f0741aea0
linamaria223 <lina090522@gmail.com>
2024-09-17 13:54:50 -0500

    suratech_4

---
+++ src/main/java/com/example/servidor_suratech4_4/ServidorSuratech44Application.java
@@ -0,0 +1,13 @@
+package com.example.servidor_suratech4_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorSuratech44Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorSuratech44Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/servidor_suratech4_4/ServidorSuratech44ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servidor_suratech4_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorSuratech44ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
