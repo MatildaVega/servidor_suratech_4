8ad6498a4ff36b372798a465d1030b4b5c7492bd
linamaria223 <lina090522@gmail.com>
2024-09-17 15:02:13 -0500

    suratech4

---
+++ src/main/java/com/example/servidor_suratech4_4/MODELOS/Paciente.java
@@ -0,0 +1,23 @@
+package com.example.servidor_suratech4_4.MODELOS;
+
+import java.time.LocalDate;
+
+public class Paciente {
+
+    private Integer id;
+    private String nombres;
+    private Integer edad;
+    private LocalDate fechaNacimiento;
+    private String documento;
+    private Integer copago;
+    private String email;
+    private String direccion;
+    private String ips;
+    private String medicoFamilia;
+
+
+
+
+
+
+}
